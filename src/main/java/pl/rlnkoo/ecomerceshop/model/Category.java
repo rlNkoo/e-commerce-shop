2ba3573c6afd590e34df638b2e83697501004f04
rlNkoo <rolakartur96@gmail.com>
2024-08-12 16:32:42 +0200

    Finished entity classes

--- src/main/java/pl/rlnkoo/ecomerceshop/model/Category.java
+++ src/main/java/pl/rlnkoo/ecomerceshop/model/Category.java
@@ -1,4 +1,26 @@
 package pl.rlnkoo.ecomerceshop.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
 }
