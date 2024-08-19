332b171d8524e0f87979490851fa302e0a0028a2
Artur Rolak <rolakartur96@gmail.com>
2024-08-19 19:00:04 +0200

    Finished CartItemService

--- src/main/java/pl/rlnkoo/ecommerceshop/service/cart/CartItemService.java
+++ src/main/java/pl/rlnkoo/ecommerceshop/service/cart/CartItemService.java
@@ -1,4 +1,81 @@
 package pl.rlnkoo.ecommerceshop.service.cart;
 
-public class CartItemService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.rlnkoo.ecommerceshop.exceptions.ResourceNotFoundException;
+import pl.rlnkoo.ecommerceshop.model.Cart;
+import pl.rlnkoo.ecommerceshop.model.CartItem;
+import pl.rlnkoo.ecommerceshop.model.Product;
+import pl.rlnkoo.ecommerceshop.repository.CartItemRepository;
+import pl.rlnkoo.ecommerceshop.repository.CartRepository;
+import pl.rlnkoo.ecommerceshop.service.product.IProductService;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService{
+
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(new CartItem());
+        if (cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Item not found"));
+    }
 }
--- src/main/java/pl/rlnkoo/ecommerceshop/service/cart/ICartItemService.java
+++ src/main/java/pl/rlnkoo/ecommerceshop/service/cart/ICartItemService.java
@@ -1,4 +1,11 @@
 package pl.rlnkoo.ecommerceshop.service.cart;
 
+import pl.rlnkoo.ecommerceshop.model.CartItem;
+
 public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
