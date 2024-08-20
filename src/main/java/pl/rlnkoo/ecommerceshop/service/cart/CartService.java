52469c4b72ced0473154b091dde356cf1a7c51ee
Artur Rolak <rolakartur96@gmail.com>
2024-08-20 18:16:13 +0200

    Finished CartItemController

--- src/main/java/pl/rlnkoo/ecommerceshop/service/cart/CartService.java
+++ src/main/java/pl/rlnkoo/ecommerceshop/service/cart/CartService.java
@@ -8,6 +8,7 @@ import pl.rlnkoo.ecommerceshop.repository.CartItemRepository;
 import pl.rlnkoo.ecommerceshop.repository.CartRepository;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class CartService implements ICartService {
 
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final AtomicLong cartIdGenerator = new AtomicLong(0);
 
     @Override
     public Cart getCart(Long id) {
@@ -37,5 +39,13 @@ public class CartService implements ICartService {
     public BigDecimal getTotalPrice(Long id) {
         Cart cart = getCart(id);
         return cart.getTotalAmount();
+    }
+
+    @Override
+    public Long initializeNewCart() {
+        Cart newCart = new Cart();
+        Long newCartId = cartIdGenerator.incrementAndGet();
+        newCart.setId(newCartId);
+        return cartRepository.save(newCart).getId();
     }
 }
